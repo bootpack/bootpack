5e1a8e50f5c031aa491e822df0fb45d3a0d83cb9
ZachTRice <contact@zachtrice.com>
2018-05-08 23:01:26 -0400

    Add initial structure & image generation tool

---
+++ tools/image-generator.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+var imgGen = require('js-image-generator');
+
+// Generate one image
+imgGen.generateImage(800, 600, 80, function(err, image) {
+    fs.writeFileSync('placeholder.jpg', image.data);
+});
+
+// Generate multiple images
+// for(var i=1;i<=10;i++){
+//     imgGen.generateImage(800, 600, 80, function(err, image) {
+//         console.log("Generating image #" +i);
+//         fs.writeFileSync('placeholder-' + i + '.jpg', image.data);
+//     });
+// }
