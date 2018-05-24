c8f22a8b75ca0228268af8735c6d652b74b121e7
ZachTRice <zachtrice@gmail.com>
2018-05-23 22:28:52 -0400

    Fix lint error to pass tests

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
+import '../css/main.css';
+
 if (process.env.NODE_ENV === 'development') {
   require('../pages/index.html'); /* Require so webpack watches changes to html file */
 }
-
-import '../css/main.css';
