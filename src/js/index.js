3381c6507d5acc4ee1e2f9b23fb1cc27db91f32d
ZachTRice <zachtrice@gmail.com>
2018-06-04 16:22:36 -0400

    Change bootstrap import to point to src files

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,24 @@
 import '../css/main.css';
 
 /* Boostrap 4 Full Bundles */
-import 'bootstrap/dist/js/bootstrap.bundle'; /* Includes popper.js */
+import 'bootstrap/src/js/bootstrap.bundle'; /* Includes popper.js */
 // import 'bootstrap'; /* Does not include popper. js */
 
 /* Bootstrap 4 Tooltip Dependency (Optional) */
 // import 'popper.js';
 
 /* Boostrap 4 Individual Components (Optional) */
-// import 'bootstrap/js/dist/alert';
-// import 'bootstrap/js/dist/button';
-// import 'bootstrap/js/dist/carousel';
-// import 'bootstrap/js/dist/collapse';
-// import 'bootstrap/js/dist/dropdown';
-// import 'bootstrap/js/dist/modal';
-// import 'bootstrap/js/dist/popover';
-// import 'bootstrap/js/dist/scrollspy';
-// import 'bootstrap/js/dist/tab';
-// import 'bootstrap/js/dist/tooltip'; /* requires popper.js */
-// import 'bootstrap/js/dist/util';
+// import 'bootstrap/js/src/alert';
+// import 'bootstrap/js/src/button';
+// import 'bootstrap/js/src/carousel';
+// import 'bootstrap/js/src/collapse';
+// import 'bootstrap/js/src/dropdown';
+// import 'bootstrap/js/src/modal';
+// import 'bootstrap/js/src/popover';
+// import 'bootstrap/js/src/scrollspy';
+// import 'bootstrap/js/src/tab';
+// import 'bootstrap/js/src/tooltip'; /* requires popper.js */
+// import 'bootstrap/js/src/util';
 
 if (process.env.NODE_ENV === 'development') {
   require('../index.html'); /* Require so webpack watches changes to html file */
