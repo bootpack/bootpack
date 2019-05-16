d5a01aa589881e358c05559e5c823d6e08d3a165
ZachTRice <zachtrice@gmail.com>
2019-05-15 22:17:15 -0400

    Add image-compression tool

---
+++ tools/compress-images.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const chalk = require('chalk');
+const compressImages = require('compress-images');
+const imageInput = './src/images-original/**/*.{jpg,JPG,jpeg,JPEG,png,svg,gif}';
+const imageOutput = './src/images/';
+
+fs.rename('./src/images', './src/images-original', function (err) {
+  if (err) {
+    console.log(chalk.red.bold('Cannot rename directory. Try removing the "images-original" directory if it exists.'));
+    throw err;
+  }
+});
+
+// Compress Images
+compressImages(imageInput, imageOutput, { compress_force: false, statistic: true, autoupdate: true }, false,
+  {
+    jpg: {
+      engine: 'jpegRecompress',
+      command: ['--accurate', '--quality', 'high', '--min', '60']
+    }
+  },
+  {
+    png: {
+      engine: 'pngquant',
+      command: ['--quality=70-90']
+    }
+  },
+  {
+    svg: {
+      engine: 'svgo',
+      command: '--multipass'
+    }
+  },
+  {
+    gif: {
+      engine: 'gifsicle',
+      command: ['--optimize']
+    }
+  },
+  function(error, completed, statistic) {
+    console.log('-------------');
+    console.log(error);
+    console.log(completed);
+    console.log(statistic);
+    console.log('-------------');
+  }
+);
