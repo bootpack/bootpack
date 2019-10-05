72976538d37fcaea53a6ab65c88dd843e941880e
Ubuntu <ubuntu@ip-172-31-19-250.us-east-2.compute.internal>
2019-10-05 17:09:39 +0000

    Added an option to specify a height and width for generated placeholder images

--- tools/generate-images.js
+++ tools/generate-images.js
@@ -1,8 +1,31 @@
 var fs = require('fs');
 var imgGen = require('js-image-generator');
 
+function GetParamaterValue(param) {
+  if(process.argv != null) {
+    var position = process.argv.indexOf(param,2);
+    if(position != -1 && process.argv.length >= position+2) {
+      var value = process.argv[position+1];
+    }
+    return value;
+  }
+  return -1;
+}
+
+var width = parseInt(GetParamaterValue("width"));
+var height = parseInt(GetParamaterValue("height"));
+
+
+if(width <= 0) {
+  console.log("Width must be a number above 0.");
+}
+
+if(height <= 0) {
+  console.log("Height must be a number above 0.");
+}
+
 // Generate one image
-imgGen.generateImage(800, 600, 80, function(error, image) {
+imgGen.generateImage(width, height, 80, function(error, image) {
   if (error) console.log(error.stack);
   fs.writeFileSync('src/images/placeholder.jpg', image.data);
 });
