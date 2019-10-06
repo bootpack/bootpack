4a9e23b00a22e9b9c9705c7169f78e0294c28699
ZachTRice <zachtrice@gmail.com>
2019-10-06 19:44:39 -0400

    Add gh-pages deploy script & update documentation

---
+++ tools/deploy.js
@@ -0,0 +1,63 @@
+const shell = require('shelljs');
+const PACKAGE_VERSION = 'v' + shell.exec('node -p "require(\'./package.json\').version"');
+
+// Deploy to gh-pages branch
+if (process.argv[3] === 'gh-pages') {
+  if (!shell.which('git')) {
+    shell.echo('Sorry, this script requires git');
+    shell.exit(1);
+  } else if (!shell.which('npm')) {
+    shell.echo('Sorry, this script requires npm');
+    shell.exit(1);
+  }
+
+  if (shell.exec('git checkout master').code !== 0) {
+    shell.echo('Error: \'git checkout master\' failed');
+    shell.exit(1);
+  }
+
+  if (shell.exec('npm install').code !== 0) {
+    shell.echo('Error: \'npm install\' failed');
+    shell.exit(1);
+  }
+
+  shell.rm('-rf', 'dist');
+
+  shell.exec('git worktree remove dist');
+
+  if (shell.exec('git worktree add dist gh-pages').code !== 0) {
+    shell.echo('Error: \'git worktree add dist gh-pages\' failed');
+    shell.exit(1);
+  }
+
+  shell.mkdir('.temp');
+  shell.cp('dist/.git', '.temp/.git');
+
+  if (shell.exec('npm run build').code !== 0) {
+    shell.echo('Error: \'npm run build\' failed');
+    shell.exit(1);
+  }
+
+  shell.cp('.temp/.git', 'dist/.git');
+
+  shell.rm('-rf', '.temp');
+
+  shell.cd('dist');
+
+  if (shell.exec('git add .').code !== 0) {
+    shell.echo('Error: \'git add .\' failed');
+    shell.exit(1);
+  }
+
+  if (shell.exec('git commit -m ' + PACKAGE_VERSION).code !== 0) {
+    shell.echo('Error: \'git commit -m ' + PACKAGE_VERSION + '\' failed');
+    shell.exit(1);
+  }
+
+  if (shell.exec('git push origin gh-pages').code !== 0) {
+    shell.echo('Error: \'git push origin gh-pages\' failed');
+    shell.exit(1);
+  }
+} else {
+  shell.exit(1);
+}
